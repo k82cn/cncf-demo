e2295f2d0e4dcf6188474f8d861b2ef3e78b9de1
Klaus Ma <klausm@nvidia.com>
2023-08-28 15:52:54 +0800

    Add bring_up_env.sh
    Signed-off-by: Klaus Ma <klausm@nvidia.com>

--- bring_up_env.sh
+++ bring_up_env.sh
@@ -2,6 +2,7 @@
 
 vagrant up
 
-vagrant upload ./install_minikube.sh /opt/install_minikube.sh
-vagrant ssh -- -t "/opt/install_minikube.sh"
+vagrant ssh -- -t "sudo mkdir -p /opt/cncf-demo && sudo chown vagrant:vagrant /opt/cncf-demo"
+vagrant upload ./install_minikube.sh /opt/cncf-demo/install_minikube.sh
+vagrant ssh -- -t "/bin/bash /opt/cncf-demo/install_minikube.sh"
 
--- install_minikube.sh
+++ install_minikube.sh
@@ -4,7 +4,10 @@
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
-minikube start
+# download kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install kubectl /usr/local/bin/kubectl
 
-minikube kubectl -- get po -A
+# start minkube
+minikube start
 
